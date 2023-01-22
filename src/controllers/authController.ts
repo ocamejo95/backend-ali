f898ccfcc7478dee3d2d6f8fba51f7ffbe9fcc78
Orel Pérez Camejo <ocamejo123@gmail.com>
2023-01-22 05:12:00 +0100

    Formateo de la clase auth

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -7,17 +7,15 @@ import { generarJWT } from "../helpers/jwt";
 import { userModel } from "../models/user";
 import { authService } from "../services/authService";
 
-
-   declare module "express-serve-static-core" {
-       interface Request {
-           uid: any;
-       }
-    }
+declare module "express-serve-static-core" {
+  interface Request {
+    uid: any;
+  }
+}
 
 @Service()
 export class AuthController {
-  constructor(private readonly authService: authService) {
-  }
+  constructor(private readonly authService: authService) {}
 
   async login(req: Request, res: Response) {
     const { password, email } = req.body;
@@ -28,47 +26,43 @@ export class AuthController {
       return res.status(400).json({ errors: errores.mapped() });
     }
 
-        try {
+    try {
+      //Verificar emial
+      const userDB = await userModel.findOne({ email });
+      if (!userDB) {
+        return res
+          .status(404)
+          .json({ message: "There was a problem with your login." });
+      }
 
-    //Verificar emial
-    const userDB = await userModel.findOne({ email });
-    if (!userDB) {
-      return res.status(404).json({ message: "User invalid!" });
-    }
+      //Verifiar password
+      const validarPassword = bcrypt.compareSync(password, userDB.password);
 
-    //Verifiar password
-    const validarPassword = bcrypt.compareSync(password, userDB.password);
+      if (!validarPassword) {
+        return res
+          .status(404)
+          .json({ message: "There was a problem with your login!" });
+      }
 
-    if (!validarPassword) {
-      return res.status(404).json({ message: "User invalid!" });
-    }
+      //Generar Token - JWT
+      const token = await generarJWT(userDB.id);
 
-    //Generar Token - JWT
-    const token = await generarJWT(userDB.id);
-
-    res.json({ token });        
-
+      res.json({ token });
     } catch (error) {
-        console.log(error);
-        res.status(500).json({message: 'Error inesperado... revisar logs'});
+      console.log(error);
+      res.status(500).json({ message: "Error inesperado... revisar logs" });
     }
-
   }
 
   async renewToken(req: Request, res: Response) {
-
     const uid = req.uid;
 
     //Generar token-jwt
     const token = await generarJWT(uid.uid);
 
-
     //Obtener usuario por uid
     const usuario = await userModel.findById(uid.uid);
- 
 
-    res.status(200).json({message: 'New token', usuario, token});
+    res.status(200).json({ message: "New token", usuario, token });
   }
 }
-
-
